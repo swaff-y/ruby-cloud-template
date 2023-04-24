b20e37711c5c716f24449edd3749f81fc9c72561
swaff-y <kyle@swaff.id.au>
2023-04-25 00:20:20 +1000

    CT-13 updated deployment script

--- .buildkite/scripts/deployment.sh
+++ .buildkite/scripts/deployment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker build --tag cloud-template --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY .
+echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}"
+docker build --tag cloud-template --build-arg "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" .
 
 docker run --rm cloud-template bundle exec rake deploy_dev
