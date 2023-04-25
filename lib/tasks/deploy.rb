d5c9edce2fad2a3ddf793edf5861f6bd1aa9a744
swaff-y <kyle@swaff.id.au>
2023-04-25 21:45:45 +1000

    CT-13

--- lib/tasks/deploy.rb
+++ lib/tasks/deploy.rb
@@ -27,6 +27,7 @@ module Tasks
 
       puts `pwd`
       puts `ls -la`
+      puts `npm install`
       File.write('serverless.yml', @serverless_yml_hash.to_yaml)
       puts `ls -la ../`
       puts `ls -la ../bin/`
