9bfa561b8ef6698ac3415e0bd68097bd5bd4de9f
swaff-y <kyle@swaff.id.au>
2023-04-26 21:20:56 +1000

    CT-13 Fixed swapped dev and prod in build scripts

--- .buildkite/scripts/deployment.sh
+++ .buildkite/scripts/deployment.sh
@@ -2,15 +2,6 @@
 
 if [ "$BUILDKITE_BRANCH" == "main"  ]
 then
-    docker build \
-    -f Dockerfile-deploy-dev \
-    --tag cloud-template-deploy \
-    --build-arg "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
-    --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
-    --build-arg "SLS_ACCESS_KEY_ID=${SLS_ACCESS_KEY_ID}" \
-    --build-arg "SLS_SECRET_ACCESS_KEY=${SLS_SECRET_ACCESS_KEY}" \
-    --build-arg "BRANCH=${BUILDKITE_BRANCH}" .
-else
   docker build \
     -f Dockerfile-deploy-prod \
     --tag cloud-template-deploy \
@@ -18,6 +9,15 @@ else
     --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
     --build-arg "SLS_ACCESS_KEY_ID=${SLS_ACCESS_KEY_ID}" \
     --build-arg "SLS_SECRET_ACCESS_KEY=${SLS_SECRET_ACCESS_KEY}" .
+else
+  docker build \
+    -f Dockerfile-deploy-dev \
+    --tag cloud-template-deploy \
+    --build-arg "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+    --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+    --build-arg "SLS_ACCESS_KEY_ID=${SLS_ACCESS_KEY_ID}" \
+    --build-arg "SLS_SECRET_ACCESS_KEY=${SLS_SECRET_ACCESS_KEY}" \
+    --build-arg "BRANCH=${BUILDKITE_BRANCH}" .
 fi
 
 docker run --rm cloud-template-deploy sls deploy
