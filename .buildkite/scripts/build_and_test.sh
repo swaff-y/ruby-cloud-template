56eefdde7ad967d089ff9cd7714325ec254398dd
swaff-y <kyle@swaff.id.au>
2023-05-03 22:53:14 +1000

    CT-1 Fixed debug stuff and unused env vars

--- .buildkite/scripts/build_and_test.sh
+++ .buildkite/scripts/build_and_test.sh
@@ -6,17 +6,13 @@ then
     -f Dockerfile-test \
     --tag cloud-template-deploy \
     --build-arg "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
-    --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
-    --build-arg "SLS_ACCESS_KEY_ID=${SLS_ACCESS_KEY_ID}" \
-    --build-arg "SLS_SECRET_ACCESS_KEY=${SLS_SECRET_ACCESS_KEY}" .
+    --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" .
 else
   docker build \
     -f Dockerfile-test \
     --tag cloud-template-deploy \
     --build-arg "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
     --build-arg "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
-    --build-arg "SLS_ACCESS_KEY_ID=${SLS_ACCESS_KEY_ID}" \
-    --build-arg "SLS_SECRET_ACCESS_KEY=${SLS_SECRET_ACCESS_KEY}" \
     --build-arg "BRANCH=${BUILDKITE_BRANCH}" .
 fi
 
