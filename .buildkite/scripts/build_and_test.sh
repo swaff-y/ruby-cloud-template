49d5f06f12c82a4f5895b208d7756695c888b42e
swaff-y <kyle@swaff.id.au>
2023-05-03 21:21:08 +1000

    CT-13 pipeline specific artifact folder

--- .buildkite/scripts/build_and_test.sh
+++ .buildkite/scripts/build_and_test.sh
@@ -26,10 +26,10 @@ docker run \
 if [ "$BUILDKITE_BRANCH" == "main"  ]
 then
   docker run \
-    --mount type=bind,source="$(pwd)"/serverless.yml,target=/app/serverless.yml \
+    --mount type=bind,source=/Users/kyleswaffield/docker/${BUILDKITE_PIPELINE_NAME}/${BUILDKITE_BRANCH}/serverless.yml,target=/app/serverless.yml \
     cloud-template-deploy bundle exec rake deploy_prod
 else
   docker run \
-    --mount type=bind,source=/Users/kyleswaffield/docker/serverless.yml,target=/app/serverless.yml \
+    --mount type=bind,source=/Users/kyleswaffield/docker/${BUILDKITE_PIPELINE_NAME}/${BUILDKITE_BRANCH}/serverless.yml,target=/app/serverless.yml \
     cloud-template-deploy bundle exec rake deploy_dev
 fi
