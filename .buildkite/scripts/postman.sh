3d4dec39943a07c71e5fee8fc3c46451abe48640
swaff-y <kyle@swaff.id.au>
2023-05-07 20:22:12 +1000

    CT-21 Postman collection

---
+++ .buildkite/scripts/postman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+      
+export UNIQUE_ID=$(cat deploy_result | grep -m 1 execute-api | sed 's|^.*\/\/\(.*\)\.ex.*$|\1|g')
+
+bundle exec rake set_unique_id
+
+postman collection run postman_collection.json > postman_result
+
+cat postman_result
+
+bundle exec rake postman
