c4d93c54ea660f81f03df7afd9eafb5806a387f5
swaff-y <kyle@swaff.id.au>
2023-05-15 22:09:02 +1000

    CT-6 Updated Specs Base spec

--- spec/models/base_spec.rb
+++ spec/models/base_spec.rb
@@ -40,14 +40,15 @@ RSpec.describe Models::Base do
 
   describe '#hash_schema' do
     let(:schema) { { west: 'string' } }
+
     before do
       allow(Validation::SchemaValidation).to receive(:validate_hash_on_schema).and_return(true)
     end
 
     context 'when mapped hash is not empty' do
-      let(:hash) { { west: 'value' } }
+      let(:hash) { { 'west' => 'value' } }
 
-      xit 'returns the mapped hash' do
+      it 'returns the mapped hash' do
         model.instance_variable_set(:@model, double(:model, schema: schema))
         expect(model.hash_schema(hash, 'post')).to eq({ west: 'value' })
       end
@@ -58,18 +59,193 @@ RSpec.describe Models::Base do
 
       it 'raises an error' do
         model.instance_variable_set(:@model, double(:model, schema: schema))
-        expect { model.hash_schema(hash, 'post') }.to raise_error 
+        expect { model.hash_schema(hash, 'post') }.to raise_error Exceptions::InvalidParametersError, 'The query parameters provided are not valid'
       end
     end
 
-    # context 'when type is post' do
-    # end
+    context 'when type is put' do
+      let(:hash) { { 'west' => 'value' } }
 
-    # context 'when type is put' do
+      it 'returns the mapped hash' do
+        model.instance_variable_set(:@model, double(:model, schema: schema))
+        expect(model.hash_schema(hash, 'put')).to eq({ west: 'value' })
+      end
+    end
 
-    # end
+    context 'when type is find' do
+      let(:hash) { { 'west' => 'value' } }
 
-    # context 'when type is find' do
-    # end
+      it 'returns the mapped hash' do
+        model.instance_variable_set(:@model, double(:model, schema: schema))
+        expect(model.hash_schema(hash, 'find')).to eq({ west: 'value' })
+      end
+    end
+  end
+
+  describe '#find_by_id' do
+    before do
+      allow(BSON).to receive(:ObjectId).and_return('Object id')
+      allow_any_instance_of(described_class).to receive(:check_collection).and_return(true)
+    end
+
+    context 'when no ID' do
+      it 'returns nil' do
+        expect(model.find_by_id).to be_nil
+      end
+    end
+
+    context 'when ID' do
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, find: ['array', 'of', 'values']))
+        expect(model.find_by_id('id')).to eq 'array'
+      end
+    end
+  end
+
+  describe '#find' do
+    before do
+      allow_any_instance_of(described_class).to receive(:check_collection).and_return(true)
+      allow_any_instance_of(described_class).to receive(:hash_schema).and_return({})
+    end
+
+    context 'when no hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(false)
+      end
+
+      it 'returns nil' do
+        expect(model.find).to be_nil
+      end
+    end
+
+    context 'when valid hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, find: ['array', 'of', 'values']))
+        expect(model.find({})).to eq(["array", "of", "values"])
+      end
+    end
+  end
+
+  describe '#post' do
+    before do
+      allow_any_instance_of(described_class).to receive(:check_collection).and_return(true)
+      allow_any_instance_of(described_class).to receive(:hash_schema).and_return({})
+    end
+
+    context 'when no hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(false)
+      end
+
+      it 'returns nil' do
+        expect(model.post).to be_nil
+      end
+    end
+
+    context 'when valid hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, insert_one: double(:insert_one, n: 1, inserted_id: 'ID')))
+        expect(model.post({})).to eq 'ID'
+      end
+    end
+
+    context 'when bad response' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, insert_one: double(:insert_one, n: 0)))
+        expect { model.post({}) }.to raise_error Exceptions::RecordNotCreatedError, 'Record could not be created'
+      end
+    end
+  end
+
+  describe '#update' do
+    before do
+      allow_any_instance_of(described_class).to receive(:check_collection).and_return(true)
+      allow_any_instance_of(described_class).to receive(:hash_schema).and_return({})
+      allow(BSON).to receive(:ObjectId).and_return('ID')
+    end
+
+    context 'when no hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(false)
+      end
+
+      it 'returns nil' do
+        expect(model.update('id')).to be_nil
+      end
+    end
+
+    context 'when valid hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, update_one: double(:update_one, n: 1, modified_count: 1)))
+        expect(model.update('id', {})).to eq 1
+      end
+    end
+
+    context 'when bad response' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, update_one: double(:update_one, n: 0)))
+        expect { model.update('id', {}) }.to raise_error Exceptions::RecordNotCreatedError, 'Error updating record'
+      end
+    end
+  end
+
+  describe '#delete' do
+    before do
+      allow_any_instance_of(described_class).to receive(:check_collection).and_return(true)
+      allow_any_instance_of(described_class).to receive(:hash_schema).and_return({})
+      allow(BSON).to receive(:ObjectId).and_return('ID')
+    end
+
+    context 'when no hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(false)
+      end
+
+      it 'returns nil' do
+        expect(model.delete).to be_nil
+      end
+    end
+
+    context 'when valid hash' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, delete_one: double(:delete_one, n: 1, deleted_count: 1)))
+        expect(model.delete('id')).to eq 1
+      end
+    end
+
+    context 'when bad response' do
+      before do
+        allow_any_instance_of(described_class).to receive(:valid_hash?).and_return(true)
+      end
+
+      it 'returns found object' do
+        model.instance_variable_set(:@collection, double(:collection, delete_one: double(:delete_one, n: 0)))
+        expect { model.delete('id') }.to raise_error Exceptions::RecordNotCreatedError, 'Error deleting record'
+      end
+    end
   end
 end
